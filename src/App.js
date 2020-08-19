5a4233509a413d17fae107d8db042ccb9fa46f56
anupam-tiwari <appleanupam@gmail.com>
2020-08-19 22:50:15 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React from 'react'
+import "bootstrap/dist/css/bootstrap.min.css"
 import './App.css';
+import {BrowserRouter as Router, Route} from "react-router-dom";
+
+
+import Navbar from "./components/navbar.component";
+import MenuList from "./components/menu-list.component";
+import EditMenu from "./components/edit-menu.component";
+import CreateMenu from "./components/create-menu.component";
+import CreateUser from "./components/create-user.component";
+
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+      <div className="container">
+    <Navbar />
+    <br/>
+    <Route path="/" exact component={MenuList} />
+    <Route path="/edit/:id" component={EditMenu} />
+    <Route path="/create" component={CreateMenu} />
+    <Route path="/user" component={CreateUser} />
     </div>
+    </Router>
   );
 }
 
